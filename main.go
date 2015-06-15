a526ce78d225f3c04fee70bdfaf98fb456ae28f1
Vaishakh Nadgir <vaishakh.nadgir@ft.com>
2015-06-15 16:18:44 +0100

    Added tests and the main file to do some basic /build-info checking

--- main.go
+++ main.go
@@ -1,9 +1,76 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"io/ioutil"
+	"path/filepath"
+	"net/http"
+	"encoding/json"
+	"strconv"
+
+	"gopkg.in/yaml.v2"
 )
 
+type HostAndPort struct {
+	Hostname string `yaml:"host"`
+	Port int `yaml:"port"`
+}
+
+type BuildInfo struct {
+	GroupId string `json:"groupId"`
+	ArtifactId string `json:"artifactId"`
+	Version string `json:"version"`
+}
+
+
 func main() {
-	log.Printf("Hello World")
+	version := flag.String("versionToCheck", "", "Expected version number to check for in the /build-info endpoint")
+
+	filename, _ := filepath.Abs("./services.yml")
+	hostAndPorts := ParseConfig(filename)
+	for _, hostAndPort := range hostAndPorts {
+		AssertVersion(hostAndPort, *version)
+	}
+}
+
+func ParseConfig(filename string) []HostAndPort {
+	yamlFile, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	var hostAndPorts []HostAndPort
+
+	err = yaml.Unmarshal(yamlFile, &hostAndPorts)
+	if err != nil {
+		panic(err)
+	}
+	return hostAndPorts
+}
+
+func AssertVersion(hostAndPort HostAndPort, version string) (bool, string) {
+	resp, err := http.Get("http://" + hostAndPort.Hostname + ":" + strconv.Itoa(hostAndPort.Port) + "/build-info")
+	if err != nil{
+		panic(err)
+	}
+
+
+	defer resp.Body.Close()
+	var deployedServiceInfo BuildInfo
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(data, &deployedServiceInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	if deployedServiceInfo.Version == version {
+		return true, "Success"
+	}else{
+		return true, "Expected: " + version + ", but found: " + deployedServiceInfo.Version
+	}
+
 }
---
+++ main_test.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"testing"
+	"net/http/httptest"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/stretchr/testify/assert"
+	"strconv"
+)
+
+var (
+	mux *http.ServeMux
+	server *httptest.Server
+)
+
+func newLocalListener(port int) net.Listener {
+	customPort := strconv.Itoa(port)
+	l, err := net.Listen("tcp", "127.0.0.1:"+customPort)
+	if err != nil {
+		if l, err = net.Listen("tcp6", "[::1]:" + customPort); err != nil {
+			panic(fmt.Sprintf("httptest: failed to listen on a port: %v", err))
+		}
+	}
+	return l
+}
+
+func newHttpTestServer(handler http.Handler, port int) *httptest.Server{
+	return &httptest.Server{
+		Listener: newLocalListener(port),
+		Config:   &http.Server{Handler: handler},
+	}
+}
+
+const (
+	randomPort = 12221
+)
+
+func setup() {
+	// test server
+	mux = http.NewServeMux()
+	server = newHttpTestServer(mux, randomPort)
+
+	server.Start()
+	fmt.Print("Running on ")
+}
+
+func teardown(){
+	server.Close()
+}
+
+func TestParsingOfConfigFile(t *testing.T) {
+	assert := assert.New(t)
+
+	hostsAndPorts := ParseConfig("./test/services.yml")
+	for _, hostPort := range hostsAndPorts {
+		assert.Equal("localhost", hostPort.Hostname)
+		assert.True(12223 -hostPort.Port >0 && 12223 -hostPort.Port <3)
+	}
+}
+
+func TestAssertionOfVersion(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/build-info", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, `{"groupId":"testing","artifactId":"testing-service","version":"1.2.3"}`)
+		},
+	)
+
+	assert := assert.New(t)
+
+	hostAndPort := HostAndPort {
+		Hostname: "localhost",
+		Port: randomPort,
+	}
+
+	correctVersion, message := AssertVersion(hostAndPort, "1.2.3")
+	assert.True(correctVersion)
+	assert.Contains("Success", message)
+}
